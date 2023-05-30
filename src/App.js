9049c944994f5f3616ae6df68b89a57a9a337691
tebibux <tebibusolomon79@gmail.com>
2023-05-30 19:07:10 +0300

    UI stated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import './css/style.css';
 
 const App = () => {
   return (
-    <h1>It took to long!</h1>
+    <div className="App">
+      <header className="header">Header</header>
+      <div className="container">
+        <div className="sidebar-left">Left Sidebar</div>
+        <main className="content">Main Content</main>
+        <div className="sidebar-right">Right Sidebar</div>
+      </div>
+      <footer className="footer">Footer</footer>
+    </div>
   );
-}
+};
 
 export default App;
