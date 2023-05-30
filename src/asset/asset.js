951e5da0b063764041906ea7eb5c22132ae80d7f
tebibux <tebibusolomon79@gmail.com>
2023-05-30 18:45:14 +0300

    price change

--- src/asset/asset.js
+++ src/asset/asset.js
@@ -15,52 +15,52 @@ const assetObject = {
 	asset1: {
 		image: img1,
 		productDisc: 'AIEYEZO Thick Frame Blue Light Glasses Men Women, Fashion Square Computer Eyeglass Anti Eyestrain',
-		productPrice: 1234
+		productPrice: 14
 	},
 	asset2: {
 		image: img2,
 		productDisc: 'COASION Classic Non-prescription Clear Lens Eyeglasses for Women Thick Square Frame Eyewear',
-		productPrice: 1234
+		productPrice: 15
 	},
 	asset3: {
 		image: img3,
 		productDisc: 'EYLRIM Classic Thick Square Frame Clear Lens Glasses for Women Men Non Prescription Eyeglasses',
-		productPrice: 1234
+		productPrice: 17
 	},
 	asset4: {
 		image: img4,
 		productDisc: "Teumire Classic Fake Nerd Glasses for Women Men Clear Lens Semi Rimless Eyeglasses",
-		productPrice: 1234
+		productPrice: 9
 	},
 	asset5: {
 		image: img5,
 		productDisc: "OCCI CHIARI Anti Blue Light Glasses for men - Computer Glasses Men - Black Eyewear Frame - Game Glasses",
-		productPrice: 1234
+		productPrice: 22
 	},
 	asset6: {
 		image: img6,
 		productDisc: "Laemzinv Blue Light Glasses for Women, Oversized Square Thick Acetate Frame, for Computer Glasses and Gaming Glasses",
-		productPrice: 1234
+		productPrice: 36
 	},
 	asset7: {
 		image: img7,
 		productDisc: "COASION Classic Non-prescription Clear Lens Eyeglasses for Women Thick Square Frame Eyewear",
-		productPrice: 1234
+		productPrice: 99
 	},
 	assert8: {
 		image: img8,
 		productDisc: "VISOONE Blue Light Blocking Glasses Rectangle Chic Preppy Look MultiColor Frame for Women Men RIVER",
-		productPrice: 1234
+		productPrice: 43
 	},
 	assert9: {
 		image: img9,
 		productDisc: "IBOANN Blue Light Glasses Women Men 3 Pack Anti Eye Strain Computer Gaming",
-		productPrice: 1234
+		productPrice: 52
 	},
 	assert10: {
 		image: img10,
 		productDisc: "ILbsidiety Square Inflated Sunglasses for Women Men Trendy Chunky Glasses Thick Frame Y2K Mask Shades UV4",
-		productPrice: 1234
+		productPrice: 6
 	}
 };
 
