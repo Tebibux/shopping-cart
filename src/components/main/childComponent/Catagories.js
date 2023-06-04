6f83903435da728849570f6e3490c45170b8bed8
tebibux <tebibusolomon79@gmail.com>
2023-06-04 18:15:41 +0200

    catagories UI done

---
+++ src/components/main/childComponent/Catagories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import assets from '../../../asset/asset';
+import uuid4 from "uuid4";
+
+const Catagories = () => {
+	// const items = Object.keys(assets).map(item => item);
+	const items = Object.keys(assets)
+
+	return (
+		<div className="catagories-container">
+			<h1 className="catagories-title">Catagories</h1>
+			<ul className="catagories">
+				{items.map(item => {
+					return (
+						<li className="item-title" key={uuid4()}>
+							{item.toUpperCase()}
+						</li>
+					)
+				}
+				)}
+			</ul>
+		</div>
+	)
+}
+export default Catagories;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Catagories from "./childComponent/Catagories";
 
 const Main = () => {
 	return (
 		<div className="container">
-        <div className="sidebar-left">Left Sidebar</div>
+        <div className="sidebar-left">
+          <Catagories />
+        </div>
         <main className="content">Main Content</main>
         <div className="sidebar-right">Right Sidebar</div>
       </div>
