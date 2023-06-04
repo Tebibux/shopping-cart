20ed85900ef412c898735311d5913153943ab23a
tebibux <tebibusolomon79@gmail.com>
2023-06-04 20:40:13 +0200

    stress break

--- src/components/main/childComponent/Items.js
+++ src/components/main/childComponent/Items.js
@@ -1,6 +1,16 @@
 import React from "react";
+import assets from '../../../asset/asset';
+import uuid4 from "uuid4";
+
 
 const Items = () => {
+	const itemsKey = Object.entries(assets);
+	// map through the items 
+	// extract item and in each item randomize from the ten item
+	// choose one item and display the choose one
+	// const itemsAsset = Object.key(itemsKey);
+	console.log(itemsKey.map(item => Object.entries(item)));
+
 	return <h1>Items</h1>
 }
 
