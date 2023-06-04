680e9c56b4b1e028149e83d6f596e7e2dc409e04
tebibux <tebibusolomon79@gmail.com>
2023-06-04 17:02:50 +0200

    footer done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+function Footer() {
+	const year = new Date();
+	return (
+		<div className="footer">
+			<div className="footer-items">
+				<h4>Tebibux &#169; {year.getFullYear()} </h4>
+				<p>
+					<a href="https://github.com/Tebibux/shopping-cart" >
+						<FaGithub />
+					</a>
+				</p>
+			</div>
 
-const Footer = () => {
-	return <div className="footer">Footer</div>
+		</div>
+	);
 }
 
 export default Footer;
