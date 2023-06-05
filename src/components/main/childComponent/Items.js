de3b7bac451f1957695ff9a3a41c83ac2b438aaa
tebibux <tebibusolomon79@gmail.com>
2023-06-05 08:27:59 +0200

    product name captured

--- src/components/main/childComponent/Items.js
+++ src/components/main/childComponent/Items.js
@@ -4,14 +4,45 @@ import uuid4 from "uuid4";
 
 
 const Items = () => {
-	const itemsKey = Object.entries(assets);
+	const assetObject = assets;
 	// map through the items 
 	// extract item and in each item randomize from the ten item
 	// choose one item and display the choose one
 	// const itemsAsset = Object.key(itemsKey);
-	console.log(itemsKey.map(item => Object.entries(item)));
+	// console.log(itemsKey.map(item => Object.entries(item)));
+	// console.log(assetObject.clothes.asset1.productDisc)
+	const ObjectKeys = Object.keys(assetObject);
+	const assetNameHolder = () => {
+		const assetArray = [];
+		const assetName = 'asset';
+		for (let i = 1; i < 11; i++) {
+			if (assetArray.includes(`${assetName}${i}`)) {
+				return;
+			} else {
+				assetArray.push(`${assetName}${i}`);
+			}
+		}
+		return assetArray;
+	};
+	// an array that holds the array with the number
+	const assetName = assetNameHolder();
 
-	return <h1>Items</h1>
+
+	return (
+		<div className="display-item">
+			{
+				ObjectKeys.map(ObjectKey => {
+					// console.log(assetPosition)
+					return (
+						<>
+							<li key={uuid4()}>{assetObject[ObjectKey][assetName[Math.floor(Math.random() * 6)]].productDisc}</li>
+						</>
+					)
+				})
+			}
+
+		</div>
+	)
 }
 
 export default Items;
