c752a57ba77cc01d479454546af43016f92ff4d5
tebibux <tebibusolomon79@gmail.com>
2023-06-08 07:41:12 +0300

    quantity selection page started

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Catagories from "./childComponent/Catagories";
 import Items from "./childComponent/Items";
 import SelectedCatagory from "./childComponent/SelectedCatagory";
