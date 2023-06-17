343f597db2d805dc53386b4b26d9edaa02741cc3
tebibux <tebibusolomon79@gmail.com>
2023-06-17 09:43:44 +0300

    reverted update

--- src/components/header/navLink.js
+++ src/components/header/navLink.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 const HeaderNavigation = ({ cartArray }) => {
 	return (
 		<div className="header-container">
 			<h1 className="logo-text">KotteShop</h1>
 			<nav className="navbar">
 				<ul className="nav-links">
-					<Link to="/cart">
-						<li className="nav-link">
-							Cart <span className="number-cart-added">
-								{cartArray.length}
-								</span>
-						</li>
-					</Link>
-
+					<li className="nav-link">
+						Cart <span className="number-cart-added">
+							{cartArray.length}
+						</span>
+					</li>
 					<li className="nav-link">About</li>
 					<li className="nav-link">Contact</li>
 				</ul>
